5b38c50251444cc31afc3c15fc74f4628e54d440
João Lucas <joaolucas@MacBook-Pro-de-Joo-Lucas.local>
2023-01-22 15:53:17 -0300

    initial commit

---
+++ NLW-Setup/scrip.js
@@ -0,0 +1,39 @@
+/*
+  Tipos de dados
+    - informações que podem ser em textos, números, booleanos (valores lógicos: verdadeiro ou falso) ou dados mais estruturais
+ */
+
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save) 
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  // const today = "01/01"
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+
+
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ["01-03"],
+//   journal: ["01-02"],
+// }
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
